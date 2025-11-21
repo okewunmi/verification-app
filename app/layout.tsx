dbddd692e399cc8a381643ef10d923c69f3550c5
Abdulafeez-okewunmi <54816692+okewunmi@users.noreply.github.com>
2025-11-21 18:35:47 +0100

    Change layout metadata for authentication system
    Updated metadata title and description for the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,4 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-//  import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: 'FTP Attendance System',
-//   description: 'Student Attendance Management',
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <head>  
-        
-//         <script src="https://unpkg.com/@digitalpersona/fingerprint/dist/fingerprint.sdk.js" ></script>
-        
-//       </head>
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
@@ -53,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'FTP Attendance System',
-  description: 'Student Attendance Management',
+  title: 'FTP Authentication System',
+  description: 'Student Verification and Attendance Management',
 };
 
 export default function RootLayout({
